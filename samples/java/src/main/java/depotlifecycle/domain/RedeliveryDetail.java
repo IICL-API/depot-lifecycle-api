02d77b40c9cb74ce299de48812fc3d58e6f04c42
James Daugherty <jdaugherty@trtn.com>
2022-11-15 08:00:25 -0500

    Ensure upgrade type is nullable

--- samples/java/src/main/java/depotlifecycle/domain/RedeliveryDetail.java
+++ samples/java/src/main/java/depotlifecycle/domain/RedeliveryDetail.java
@@ -62,7 +62,7 @@ public class RedeliveryDetail {
     String grade;
 
     @Schema(description = "an indicator for the upgrades applied to units on this detail.\n\n`FG` - Food grade\n\n`ML` - Malt\n\n`DB` - Dairy Board\n\n`EV` - Evian\n\n`WH` - Whiskey\n\n`SU` - Sugar\n\n`CF` - Coffee\n\n`TB` - Tobacco\n\n`MC` - Milk cartons\n\n`MP` - Milk powder\n\n`AM` - Ammunition\n\n`CH` - Cotton/Hay\n\n`TE` - Tea\n\n`FT` - Flexitank", allowableValues = {"FG", "ML", "DB", "EV", "WH", "SU", "CF", "TB", "MC", "MP", "AM", "CH", "TE", "FT"}, required = false, example = "AM", maxLength = 2)
-    @Column(length = 2)
+    @Column(nullable = true, length = 2)
     String upgradeType;
 
     @Schema(description = "the specific units for this redelivery if defined, if not, assumed blanket (any unit matching criteria can be tied up to the quantity limit of this detail)")
