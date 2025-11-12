0d42a02cea102267c207701723aac6c710feb72e
James Daugherty <jdaugherty@trtn.com>
2025-11-11 22:12:39 -0500

    Update DV to local currency

--- samples/java/src/main/java/depotlifecycle/controllers/api/repair/EstimateController.java
+++ samples/java/src/main/java/depotlifecycle/controllers/api/repair/EstimateController.java
@@ -154,7 +154,7 @@ public class EstimateController {
         allocation.setCustomerTotal(estimate.getPartyTotal(EstimateLineItemParty.U));
         allocation.setCtl(false); //assume not a CTL for demo purposes
         allocation.setComments(estimate.getComments());//Assume the returned comments are the same for demo
-        allocation.setDepreciatedValueUSD(null); // do not return the depreciated value for demo purposes
+        allocation.setDepreciatedValue(null); // do not return the depreciated value for demo purposes
 
         PreliminaryDecision preliminaryDecision = new PreliminaryDecision();
         preliminaryDecision.setRecommendation("FIX");
@@ -292,7 +292,7 @@ public class EstimateController {
         allocation.setCustomerTotal(estimate.getPartyTotal(EstimateLineItemParty.U));
         allocation.setCtl(false); //assume not a CTL for demo purposes
         allocation.setComments(estimate.getComments());//Assume the returned comments are the same for demo
-        allocation.setDepreciatedValueUSD(null); // do not return the depreciated value for demo purposes
+        allocation.setDepreciatedValue(null); // do not return the depreciated value for demo purposes
 
         PreliminaryDecision preliminaryDecision = new PreliminaryDecision();
         preliminaryDecision.setRecommendation("FIX");
--- samples/java/src/main/java/depotlifecycle/domain/repair/EstimateAllocation.java
+++ samples/java/src/main/java/depotlifecycle/domain/repair/EstimateAllocation.java
@@ -71,9 +71,9 @@ public class EstimateAllocation {
     @Column(nullable = false)
     BigDecimal insuranceTotal;
 
-    @Schema(type = "number", format = "double", minimum = "0.0", example = "1151.84", description = "the depreciated value of the container in USD", required = false, nullable = true)
+    @Schema(type = "number", format = "double", minimum = "0.0", example = "1151.84", description = "the depreciated value of the container", required = false, nullable = true)
     @Column(nullable = true)
-    BigDecimal depreciatedValueUSD;
+    BigDecimal depreciatedValue;
 
     @Schema(type = "boolean", description = "indicates if the estimate causes the unit to be a constructive total lost", example = "false", required = true, nullable = false)
     @Column(nullable = false)
